8286fd895d5e4da54f6a20501cbc46df53942153
狼牙 <langya466@gmail.com>
2025-05-11 10:30:29 +0800

    🎨 优化按钮

--- src/components/custom/button.vue
+++ src/components/custom/button.vue
@@ -7,27 +7,23 @@ const router = useRouter()
 
 const handleClick = () => {
 	if (!props.is_external) {
-		router.push({
-			path: props.href
-		})
-
+		router.push({ path: props.href })
 		return
 	}
-
 	showRedirectExternalWebsiteWarningModal(props.href)
 }
 </script>
 
 <template>
-	<n-button @click="handleClick">
+	<n-button @click="handleClick" class="w-full sm:w-auto max-w-full">
 		<template #icon>
 			<slot name="icon">
-				<Component :is="icon"/>
+				<Component :is="icon" />
 			</slot>
 		</template>
 
 		<slot name="content">
-			<div class="overflow-auto">
+			<div class="text-sm sm:text-base leading-snug break-words whitespace-normal">
 				<span>{{ props.name }}</span>
 			</div>
 		</slot>
