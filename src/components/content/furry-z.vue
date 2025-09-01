91fe68446dd13733a26d4ca2962190aea9fe47e9
渣渣120 <WOSHIZHAZHA120@qq.com>
2025-09-02 00:22:29 +0800

    :sparkles: 新增 `Z` 的设定展示

---
+++ src/components/content/furry-z.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import license_32c9a19123e26cc595ffbdcaf07a9cbd42a760ce from '~/assets/license_32c9a19123e26cc595ffbdcaf07a9cbd42a760ce.webp'
+</script>
+
+<template>
+	<n-card size="small" title="版权">
+		<n-image :src="license_32c9a19123e26cc595ffbdcaf07a9cbd42a760ce" :img-props="{ class: 'w-full' }"/>
+	</n-card>
+</template>
---
+++ src/contents/furry/z.md
@@ -0,0 +1,10 @@
+---
+code: z
+name: Z
+description: 头顶有着专属于渣渣的 Z 图案
+image_hash: 172f28822cb677d72b99f8839c0aecc55ac82d48
+animal: 狗
+---
+
+::furry-z
+::
