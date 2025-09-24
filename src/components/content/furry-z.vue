80181093ddc728b03c16f0b94396d8744d8a620f
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2025-09-24 12:56:13 +0800

    :lipstick: 优化设计和排版

--- src/components/content/furry-z.vue
+++ src/components/content/furry-z.vue
@@ -8,11 +8,13 @@ import license_32c9a19123e26cc595ffbdcaf07a9cbd42a760ce from '~/assets/license_3
 			<n-flex vertical>
 				<iframe class="w-full border-none aspect-ratio-video" src="https://player.bilibili.com/player.html?aid=115178574184785&autoplay=0"/>
 
-				<n-flex :size="0">
-					<n-text type="warning">制作者: 栗糖</n-text>
-					<n-divider vertical/>
-					<n-text type="info">欧奎家兽装工作室 (群号: 1040074195)</n-text>
-				</n-flex>
+				<div class="fw-bold">
+					<n-flex :size="0" align="center">
+						<n-text type="warning">制作者: 栗糖</n-text>
+						<n-divider vertical/>
+						<n-text type="info">欧奎家兽装工作室 (群号: 1040074195)</n-text>
+					</n-flex>
+				</div>
 			</n-flex>
 		</n-card>
 
