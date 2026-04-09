52b2f5aea012482a2e2425da47b5ff956e5fca6f
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-04-09 22:45:18 +0800

    :heavy_plus_sign: :wrench: 安装 `Naive UI` 及自动导入并配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
+import AutoImport from "unplugin-auto-import/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+
 export default defineNuxtConfig({
     compatibilityDate: "latest",
     ssr: false,
     modules: ["@unocss/nuxt"],
+    vite: {
+        plugins: [
+            AutoImport({
+                imports: [
+                    {
+                        "naive-ui": [
+                            "useDialog",
+                            "useMessage",
+                            "useModal",
+                            "useNotification",
+                            "useLoadingBar",
+                        ],
+                    },
+                ],
+            }),
+            Components({
+                resolvers: [NaiveUiResolver()],
+            }),
+        ],
+    },
 });
