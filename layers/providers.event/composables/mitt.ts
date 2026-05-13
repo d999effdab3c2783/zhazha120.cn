78d1e8c35415d11a6f2551d5399470deaeb8c292
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-05-13 21:00:13 +0800

    :sparkles: 新增 `mitt` 相关组合式方法

---
+++ layers/providers.event/composables/mitt.ts
@@ -0,0 +1,16 @@
+import mitt from "#layers/providers.event/shared/mitt";
+
+export const useMitt = () => {
+    const listen = (type: string, handler: (...args: unknown[]) => void) => {
+        mitt.on(type, handler);
+
+        onScopeDispose(() => {
+            mitt.off(type, handler);
+        });
+    };
+
+    return {
+        instance: mitt,
+        listen,
+    };
+};
