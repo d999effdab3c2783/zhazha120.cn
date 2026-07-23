b4faeef91b0d9f2bf1fa98ebc795ed06c90c1a1c
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-07-24 00:58:24 +0800

    :sparkles: 完善开发简介

---
+++ layers/pages.dev/app/components/sections/home/2/dev/description.vue
@@ -0,0 +1,5 @@
+<template>
+    <n-alert :show-icon="false">
+        <SectionsDevDescription />
+    </n-alert>
+</template>
--- layers/pages.dev/app/components/sections/home/2/dev/index.vue
+++ layers/pages.dev/app/components/sections/home/2/dev/index.vue
@@ -2,6 +2,7 @@
     <n-divider>开发</n-divider>
 
     <n-flex class="w-full" size="small" vertical>
+        <SectionsHome2DevDescription />
         <SectionsHome2DevCodetime />
     </n-flex>
 </template>
