43c3e7b6f101687d62d1cb1eaf92c0f0f9ad9c20
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-07-24 01:04:15 +0800

    :lipstick: 优化移动端排版

--- layers/pages.dev/app/components/sections/dev/codetime.vue
+++ layers/pages.dev/app/components/sections/dev/codetime.vue
@@ -18,38 +18,42 @@
     <n-flex align="center" size="small" vertical>
         <n-image :src="processWidget(appConfig.dev.codetime.widgets.calendar)" />
 
-        <n-flex :wrap="isMobile" size="small">
+        <n-flex :vertical="isMobile" size="small">
             <n-image :src="processWidget(appConfig.dev.codetime.widgets.trend)" />
 
             <n-element class="flex-1">
-                <n-tabs animated>
+                <n-tabs :justify-content="isMobile ? 'center' : undefined" animated>
                     <n-tab-pane name="按语言">
-                        <n-image
-                            :src="processWidget(appConfig.dev.codetime.widgets.top.languages)"
-                        />
+                        <n-flex :justify="isMobile ? 'center' : undefined">
+                            <n-image
+                                :src="processWidget(appConfig.dev.codetime.widgets.top.languages)"
+                            />
+                        </n-flex>
                     </n-tab-pane>
 
                     <n-tab-pane name="按项目">
-                        <n-image
-                            :src="processWidget(appConfig.dev.codetime.widgets.top.projects)"
-                        />
+                        <n-flex :justify="isMobile ? 'center' : undefined">
+                            <n-image
+                                :src="processWidget(appConfig.dev.codetime.widgets.top.projects)"
+                            />
+                        </n-flex>
                     </n-tab-pane>
                 </n-tabs>
             </n-element>
         </n-flex>
 
-        <n-flex justify="space-evenly" size="small">
+        <n-flex :vertical="isMobile" align="center" justify="space-evenly" size="small">
             <n-image :src="processWidget(appConfig.dev.codetime.widgets.status)" />
             <n-image :src="processWidget(appConfig.dev.codetime.widgets.usage)" />
         </n-flex>
 
-        <n-flex justify="space-evenly" size="small">
+        <n-flex :vertical="isMobile" align="center" justify="space-evenly" size="small">
             <n-image :src="processWidget(appConfig.dev.codetime.widgets.badge.coding_time)" />
             <n-image :src="processWidget(appConfig.dev.codetime.widgets.badge.tokens)" />
         </n-flex>
 
         <n-element class="mt-4">
-            <n-flex vertical align="center" :size="0">
+            <n-flex :size="0" align="center" vertical>
                 <n-text :depth="3">仅供参考</n-text>
 
                 <naive-redirector-wrapper #="{ href, redirect }" href="https://codetime.dev">
