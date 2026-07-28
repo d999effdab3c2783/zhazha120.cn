47a001a4bcb127a1487334ffb68145b71222163f
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-07-28 17:55:05 +0800

    :lipstick: 优化文案

--- layers/pages.dev/app/components/sections/home/2/dev/index.vue
+++ layers/pages.dev/app/components/sections/home/2/dev/index.vue
@@ -9,7 +9,7 @@
             <SectionsHome2DevCodetime />
             <SectionsHome2DevStacks />
 
-            <n-divider>工作经历</n-divider>
+            <n-divider>技术履历</n-divider>
 
             <SectionsHome2DevOrganizations />
         </n-flex>
