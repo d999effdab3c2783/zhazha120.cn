fb668f68ebf0c177eb2c7566a80e09a81443921d
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-08-03 02:12:01 +0800

    :lipstick: 优化页头

--- layers/providers.layouts/app/layouts/subpage.vue
+++ layers/providers.layouts/app/layouts/subpage.vue
@@ -21,10 +21,19 @@
     <NuxtLayout name="default">
         <NuxtLayout name="container">
             <n-flex size="large" vertical>
-                <n-page-header
-                    :title="String(title ?? $route.name ?? $route.fullPath)"
-                    @back="handleBack"
-                />
+                <n-page-header @back="handleBack">
+                    <template #title>
+                        <n-breadcrumb>
+                            <template
+                                v-for="part in String(
+                                    title ?? $route.name ?? $route.fullPath,
+                                ).split('|')"
+                            >
+                                <n-breadcrumb-item>{{ part.trim() }}</n-breadcrumb-item>
+                            </template>
+                        </n-breadcrumb>
+                    </template>
+                </n-page-header>
 
                 <n-element>
                     <slot />
