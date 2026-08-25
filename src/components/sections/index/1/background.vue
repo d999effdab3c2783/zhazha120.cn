acebccc9af1cfb33b402725817c8d3383578dbe9
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-08-25 21:23:05 +0800

    :zap: 当离开页面或背景容器不可见时移除背景

--- src/components/sections/index/1/background.vue
+++ src/components/sections/index/1/background.vue
@@ -3,15 +3,24 @@
 	import VueBitsDotGrid from '@/components/vue-bits/dot-grid.vue';
 
 	const themeVars = useThemeVars();
+	const containerRef = useTemplateRef<ComponentPublicInstance>('containerRef');
+	const pageLeave = usePageLeave();
+	const elementVisibility = useElementVisibility(containerRef);
+
+	const show = computed(() => {
+		return !pageLeave.value && elementVisibility.value;
+	});
 </script>
 
 <template>
-	<n-element class="size-full absolute inset-0">
-		<VueBitsDotGrid
-			:active-color="themeVars.primaryColor"
-			:base-color="themeVars.hoverColor"
-			:dot-size="6"
-			:gap="18"
-		/>
+	<n-element ref="containerRef" class="size-full absolute inset-0">
+		<template v-if="show">
+			<VueBitsDotGrid
+				:active-color="themeVars.primaryColor"
+				:base-color="themeVars.hoverColor"
+				:dot-size="6"
+				:gap="18"
+			/>
+		</template>
 	</n-element>
 </template>
