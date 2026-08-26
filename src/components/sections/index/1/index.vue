e7ff0a173156799e1be5af393a60b6c8e9f6d235
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-08-26 22:27:16 +0800

    :art: 优化组件名

--- src/components/sections/index/1/index.vue
+++ src/components/sections/index/1/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<n-element class="h-screen relative">
 		<sections-index-1-background />
-		<sections-index-1-intro />
+		<sections-index-1-profile />
 		<sections-index-1-down-indicator />
 	</n-element>
 </template>
