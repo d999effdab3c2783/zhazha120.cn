602f77784d6efa1c1c31b78b1272d95e5e98241a
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-08-29 22:21:37 +0800

    :lipstick: 优化设计

--- src/components/sections/dev/competitions.vue
+++ src/components/sections/dev/competitions.vue
@@ -1,21 +1,37 @@
 <script lang="ts" setup>
 	import { isNonNullish } from 'remeda';
 
+	import type { Competition } from '@/data/dev/competitions';
+
 	import CustomRedirectWrapper from '@/components/custom/redirect-wrapper.vue';
 	import competitionsData from '@/data/dev/competitions' with { type: 'macro' };
 
 	const { isMobile } = useResponsive();
+	const { define: CompetitionDefine, reuse: Competition } = createReusableTemplate<Competition>();
 </script>
 
 <template>
+	<CompetitionDefine>
+		<template #default="{ name, group, award }">
+			<n-element class="leading-tight">
+				<n-flex :align="isMobile ? 'start' : 'center'" :vertical="isMobile" size="small">
+					<n-text class="text-current fw-bold">{{ name }}</n-text>
+					<n-text :depth="3" class="text-[.8em]">{{ group }}</n-text>
+					<n-text class="fw-bold">{{ award }}</n-text>
+				</n-flex>
+			</n-element>
+		</template>
+	</CompetitionDefine>
+
 	<n-list>
-		<template v-for="{ name, year, href } in competitionsData">
+		<template v-for="competition in competitionsData">
 			<n-list-item>
 				<n-flex :size="0" :vertical="isMobile" justify="space-between">
-					<template v-if="isNonNullish(href)">
-						<custom-redirect-wrapper :href="href">
+					<template v-if="isNonNullish(competition.href)">
+						<custom-redirect-wrapper :href="competition.href">
 							<template #default="{ redirect, aProps }">
 								<n-button
+									class="w-fit"
 									size="small"
 									tag="a"
 									text
@@ -23,23 +39,19 @@
 									v-bind="aProps"
 									@click.prevent="redirect"
 								>
-									<n-text class="text-(current start wrap) leading-tight fw-bold">
-										{{ name }}
-									</n-text>
+									<Competition v-bind="competition" />
 								</n-button>
 							</template>
 						</custom-redirect-wrapper>
 					</template>
 
 					<template v-else>
-						<n-text class="text-(current start wrap) leading-tight fw-bold">
-							{{ name }}
-						</n-text>
+						<Competition v-bind="competition" />
 					</template>
 
 					<n-flex justify="end">
 						<n-text :depth="3" class="text-([.8em] nowrap)">
-							{{ year }}
+							{{ competition.year }}
 						</n-text>
 					</n-flex>
 				</n-flex>
--- src/data/dev/competitions.ts
+++ src/data/dev/competitions.ts
@@ -1,42 +1,69 @@
 export type Competition = {
 	readonly name: string;
+	readonly award: string;
+
+	readonly group?: string;
 	readonly year?: number;
 	readonly href?: string;
 };
 
 export default [
 	{
-		name: ['蓝桥杯 第十六届', '软件赛 | 职业院校组 | 省赛', 'Web 应用开发', '一等奖'].join(' '),
+		name: '蓝桥杯 第十六届',
+		award: '一等奖',
+
+		group: '软件赛 | Web 应用开发 | 职业院校组 | 省赛',
 		year: 2025,
 	},
 	{
-		name: ['蓝桥杯 第十六届', '软件赛 | 职业院校组 | 国赛', 'Web 应用开发', '一等奖'].join(' '),
+		name: '蓝桥杯 第十六届',
+		award: '一等奖',
+
+		group: '软件赛 | Web 应用开发 | 职业院校组 | 国赛',
 		year: 2025,
 		href: 'https://lanqiao.cn/cup-sixteen',
 	},
 	{
-		name: ['江西省 职业院校技能大赛', '高职组 个人赛', 'Web 应用软件开发', '一等奖'].join(' '),
+		name: '江西省 职业院校技能大赛',
+		award: '一等奖',
+
+		group: 'Web 应用软件开发 | 高职组 | 个人赛',
 		year: 2025,
 		href: 'https://zs.jxeduyun.com/certifiDetail?certificatesid=26010815212809383571',
 	},
 	{
-		name: ['蓝桥杯 第十七届', '软件赛 | 职业院校组 | 省赛', 'Web 应用开发', '一等奖'].join(' '),
+		name: '蓝桥杯 第十七届',
+		award: '一等奖',
+
+		group: '软件赛 | Web 应用开发 | 职业院校组 | 省赛',
 		year: 2026,
 	},
 	{
-		name: ['江西应用工程职业学院 职业教育活动周', '校内赛', 'Web', '一等奖'].join(' '),
+		name: '江西应用工程职业学院 职业教育活动周',
+		award: '一等奖',
+
+		group: 'Web | 校内赛',
 		year: 2026,
 	},
 	{
-		name: ['计算机设计大赛', '省赛', '软件应用与开发 | Web 应用与开发', '二等奖'].join(' '),
+		name: '计算机设计大赛',
+		award: '二等奖',
+
+		group: '软件应用与开发 | Web 应用与开发 | 省赛',
 		year: 2026,
 	},
 	{
-		name: ['蓝桥杯 第十七届', '软件赛 | 职业院校组 | 国赛', 'Web 应用开发', '一等奖 (第一名)'].join(' '),
+		name: '蓝桥杯 第十七届',
+		award: '一等奖 (第一名)',
+
+		group: '软件赛 | Web 应用开发 | 职业院校组 | 国赛',
 		year: 2026,
 	},
 	{
-		name: ['计算机设计大赛', '国赛', '软件应用与开发 | Web 应用与开发', '二等奖'].join(' '),
+		name: '计算机设计大赛',
+		award: '二等奖',
+
+		group: '软件应用与开发 | Web 应用与开发 | 国赛',
 		year: 2026,
 	},
 ] satisfies Competition[];
