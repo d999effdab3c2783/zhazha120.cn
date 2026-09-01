8949ae41d01c297a2e10975d7df6349f52f79f3e
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-09-01 23:16:52 +0800

    :sparkles: 新增快速导航

--- src/components/sections/index/2/index.vue
+++ src/components/sections/index/2/index.vue
@@ -1,5 +1,6 @@
 <template>
 	<custom-naive-ui-vertical-stack :size="0">
+		<sections-index-2-portals />
 		<sections-index-2-self />
 		<sections-index-2-dev />
 		<sections-index-2-misc />
---
+++ src/components/sections/index/2/portals.vue
@@ -0,0 +1,10 @@
+<template>
+	<n-element class="pt-30 px-2 container mx-auto">
+		<n-card size="small" title="快速导航">
+			<custom-naive-ui-horizontal-stack>
+				<custom-naive-ui-redirect-button href="/self" tag="a">本体</custom-naive-ui-redirect-button>
+				<custom-naive-ui-redirect-button href="/dev" tag="a">开发</custom-naive-ui-redirect-button>
+			</custom-naive-ui-horizontal-stack>
+		</n-card>
+	</n-element>
+</template>
