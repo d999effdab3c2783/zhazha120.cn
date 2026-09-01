5cf0ab8968f0be60a40d4aa86161eab85a3fe183
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-09-01 23:00:21 +0800

    :sparkles: 新增 `dev` 页面

---
+++ src/components/pages/dev.vue
@@ -0,0 +1,40 @@
+<template>
+	<custom-naive-ui-vertical-stack>
+		<sections-dev-description />
+
+		<n-divider />
+
+		<n-card size="small" title="态势">
+			<sections-dev-codetime />
+		</n-card>
+
+		<n-divider>技术栈</n-divider>
+
+		<sections-dev-stacks />
+
+		<n-divider>技术履历</n-divider>
+
+		<n-card size="small" title="加入的组织">
+			<sections-dev-organizations />
+		</n-card>
+
+		<n-card size="small" title="做过的项目">
+			<sections-dev-projects />
+		</n-card>
+
+		<n-divider>技术挑战</n-divider>
+
+		<n-card size="small" title="打过的比赛">
+			<sections-dev-competitions />
+		</n-card>
+	</custom-naive-ui-vertical-stack>
+</template>
+
+<route lang="json5">
+{
+	meta: {
+		title: '开发',
+		layout: 'subpage',
+	},
+}
+</route>
