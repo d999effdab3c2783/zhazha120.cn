3f32e2ef57fd692d03b9c93ebabeec98363a4717
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-09-03 00:08:01 +0800

    :sparkles: 新增 `misc` 页面

---
+++ src/components/pages/misc.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+	definePage({
+		meta: {
+			title: '杂项',
+			layout: 'subpage',
+		},
+	});
+</script>
+
+<template>
+	<custom-naive-ui-vertical-stack>
+		<n-card content-class="!pb-0" size="small" title="友情链接">
+			<sections-misc-friend-links />
+		</n-card>
+	</custom-naive-ui-vertical-stack>
+</template>
