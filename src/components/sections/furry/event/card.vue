23992dc2c070c94077c2e8c716aea2e942a1da67
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-09-04 01:30:27 +0800

    :lipstick: 优化设计

--- src/components/sections/furry/event/card.vue
+++ src/components/sections/furry/event/card.vue
@@ -5,6 +5,8 @@
 
 	const props = defineProps<EventEntry>();
 
+	const { isMobile } = useResponsive();
+
 	const detailRoutePath = computed(() => {
 		if (isNonNullish(props.slug) && isNonNullish(props.year)) {
 			return `/furry/events/${props.slug}-${props.year}`;
@@ -21,62 +23,76 @@
 		<n-element class="mt-2">
 			<custom-naive-ui-auto-stack justify="space-between">
 				<template v-if="isNonNullish(slug) && isNonNullish(detailRoutePath) && $route.path !== detailRoutePath">
-					<custom-naive-ui-redirect-button :href="detailRoutePath" tag="a" text type="primary">
-						<custom-naive-ui-horizontal-stack align="center">
-							<template v-if="isNonNullish(year)">
-								<n-text>{{ year }}</n-text>
-							</template>
-
-							<custom-naive-ui-text-stack class="leading-tight">
-								<n-text class="text-(current [1.2em]) fw-bold">{{ name }}</n-text>
-
-								<template v-if="isNonNullish(slug)">
-									<n-text :depth="3">{{ slug }}</n-text>
+					<custom-naive-ui-redirect-button
+						:href="detailRoutePath"
+						class="size-fit"
+						tag="a"
+						text
+						type="primary"
+					>
+						<custom-naive-ui-text-stack>
+							<custom-naive-ui-auto-stack :align="isMobile ? undefined : 'center'">
+								<template v-if="isNonNullish(year)">
+									<n-text>{{ year }}</n-text>
 								</template>
-							</custom-naive-ui-text-stack>
-						</custom-naive-ui-horizontal-stack>
+
+								<custom-naive-ui-text-stack class="leading-tight">
+									<n-text class="text-(current [1.2em]) fw-bold">{{ name }}</n-text>
+
+									<template v-if="isNonNullish(slug)">
+										<n-text :depth="3">{{ slug }}</n-text>
+									</template>
+								</custom-naive-ui-text-stack>
+							</custom-naive-ui-auto-stack>
+						</custom-naive-ui-text-stack>
 					</custom-naive-ui-redirect-button>
 				</template>
 
 				<template v-else-if="isNonNullish(href)">
-					<custom-naive-ui-redirect-button :href="href" tag="a" text type="primary">
-						<custom-naive-ui-horizontal-stack align="center">
+					<custom-naive-ui-redirect-button :href="href" class="size-fit" tag="a" text type="primary">
+						<custom-naive-ui-text-stack>
+							<custom-naive-ui-auto-stack :align="isMobile ? undefined : 'center'">
+								<template v-if="isNonNullish(year)">
+									<n-text>{{ year }}</n-text>
+								</template>
+
+								<custom-naive-ui-text-stack class="leading-tight">
+									<n-text class="text-(current [1.2em]) fw-bold">{{ fullName ?? name }}</n-text>
+
+									<template v-if="isNonNullish(slug)">
+										<n-text :depth="3">{{ slug }}</n-text>
+									</template>
+								</custom-naive-ui-text-stack>
+							</custom-naive-ui-auto-stack>
+						</custom-naive-ui-text-stack>
+					</custom-naive-ui-redirect-button>
+				</template>
+
+				<template v-else>
+					<custom-naive-ui-text-stack>
+						<custom-naive-ui-auto-stack :align="isMobile ? undefined : 'center'">
 							<template v-if="isNonNullish(year)">
 								<n-text>{{ year }}</n-text>
 							</template>
 
 							<custom-naive-ui-text-stack class="leading-tight">
-								<n-text class="text-(current [1.2em]) fw-bold">{{ fullName ?? name }}</n-text>
+								<n-text class="text-[1.2em]">{{ name }}</n-text>
 
 								<template v-if="isNonNullish(slug)">
 									<n-text :depth="3">{{ slug }}</n-text>
 								</template>
 							</custom-naive-ui-text-stack>
-						</custom-naive-ui-horizontal-stack>
-					</custom-naive-ui-redirect-button>
+						</custom-naive-ui-auto-stack>
+					</custom-naive-ui-text-stack>
 				</template>
 
-				<template v-else>
-					<custom-naive-ui-horizontal-stack align="center">
-						<template v-if="isNonNullish(year)">
-							<n-text>{{ year }}</n-text>
-						</template>
-
-						<custom-naive-ui-text-stack class="leading-tight">
-							<n-text class="text-[1.2em]">{{ name }}</n-text>
-
-							<template v-if="isNonNullish(slug)">
-								<n-text :depth="3">{{ slug }}</n-text>
-							</template>
-						</custom-naive-ui-text-stack>
-					</custom-naive-ui-horizontal-stack>
-				</template>
-
-				<n-text :depth="3" class="text-[.8em]">
-					{{ new Date(startDate).toLocaleDateString() }}
-					-
-					{{ new Date(endDate).toLocaleDateString() }}
-				</n-text>
+				<custom-naive-ui-position placement="center-right">
+					<n-text :depth="3" class="text-[.8em]">
+						{{ new Date(startDate).toLocaleDateString() }}
+						-
+						{{ new Date(endDate).toLocaleDateString() }}
+					</n-text>
+				</custom-naive-ui-position>
 			</custom-naive-ui-auto-stack>
 		</n-element>
 	</n-card>
