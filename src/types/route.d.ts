83f58b6383c373d79ecae0c1734ff91483744e33
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-09-04 23:55:51 +0800

    :bug: 修复 `vue-router` 类型覆盖问题

--- src/types/route.d.ts
+++ src/types/route.d.ts
@@ -1,6 +1,12 @@
+// oxlint-disable-next-line import/no-unassigned-import
+import 'vue-router'
+
 declare module 'vue-router' {
 	interface RouteMeta {
 		readonly title?: string
 		readonly layout?: 'default' | 'wrapper' | 'app' | 'subpage'
 	}
 }
+
+// oxlint-disable-next-line unicorn/require-module-specifiers
+export {}
