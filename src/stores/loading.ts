de3043073dc9e8ed5e137732ad5995fc81e6b3be
WOSHIZHAZHA120 <WOSHIZHAZHA120@qq.com>
2026-09-08 00:38:27 +0800

    :sparkles: 完善 `loading` 逻辑

--- src/stores/loading.ts
+++ src/stores/loading.ts
@@ -1,8 +1,34 @@
+import type { UseTimeoutFnReturn } from '@vueuse/core'
+
+import { isNonNullish } from 'remeda'
+
 export const useLoadingStore = defineStore('loading', () => {
 	const counter = shallowRef(0)
+	const status = shallowRef(false)
+
+	let showTimeoutFn: UseTimeoutFnReturn<() => void> | undefined
+	let hideTimeoutFn: UseTimeoutFnReturn<() => void> | undefined
+
+	const resetTimeout = () => {
+		if (isNonNullish(showTimeoutFn) && showTimeoutFn.isPending.value) {
+			showTimeoutFn.stop()
+		}
+
+		if (isNonNullish(hideTimeoutFn) && hideTimeoutFn.isPending.value) {
+			hideTimeoutFn.stop()
+		}
+	}
 
 	const show = () => {
 		counter.value++
+
+		resetTimeout()
+
+		if (0 < counter.value && !status.value) {
+			showTimeoutFn = useTimeoutFn(() => {
+				status.value = true
+			}, 200)
+		}
 	}
 
 	const hide = () => {
@@ -11,16 +37,20 @@ export const useLoadingStore = defineStore('loading', () => {
 		}
 
 		counter.value--
-	}
 
-	const status = computed(() => {
-		return 0 < counter.value
-	})
+		if (0 >= counter.value) {
+			resetTimeout()
+
+			hideTimeoutFn = useTimeoutFn(() => {
+				status.value = false
+			}, 100)
+		}
+	}
 
 	return {
 		show,
 		hide,
 
-		status
+		status: readonly(status)
 	}
 })
